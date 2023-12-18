b1c8a1bf235422b3946c07cd1d866288f33cfc56
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2023-12-18 05:39:00 +0100

    try to add string and binary, compile warning but executable, no response from the server

--- projet/server.c
+++ projet/server.c
@@ -5,39 +5,60 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <ctype.h>
+
 
 #define PORT 6379
 #define BUFFER_SIZE 1024
-#define MAX_KEY_SIZE 256
+//#define MAX_KEY_SIZE 256
 #define HASH_TABLE_SIZE 1024
 
 typedef struct node {
-    char key[MAX_KEY_SIZE];
-    char* value;
+    char* key;
+    void* value;
+    size_t key_size;
+    size_t value_size;
     struct node* next;
 } node_t;
 
 node_t* hashTable[HASH_TABLE_SIZE];
 
-unsigned int hash(const char* key) {
+unsigned int hash(const char* key, size_t key_size) {
     unsigned long int value = 0;
-    while (*key != '\0') {
+    for (size_t i = 0; i < key_size; ++i) {
         value = value * 37 + *key++;
     }
     return value % HASH_TABLE_SIZE;
 }
 
-void setKeyValue(const char* key, const char* value) {
-    unsigned int index = hash(key);
+unsigned int hashBinary(const char* key, size_t len) {
+    unsigned long int value = 0;
+    for (size_t i = 0; i < len; ++i) {
+        value = value * 37 + key[i];
+    }
+    return value % HASH_TABLE_SIZE;
+}
+
+void setKeyValue(const char* key, size_t key_size, const void* value, size_t value_size) {
+    unsigned int index = hash(key, key_size);
     node_t* node = hashTable[index];
 
-    while (node != NULL && strncmp(node->key, key, MAX_KEY_SIZE) != 0) {
+    while (node != NULL && (node->key_size != key_size || strncmp(node->key, key, key_size) != 0)) {
         node = node->next;
     }
 
-    if (node != NULL) {
+    if (node != NULL) 
+    {
         free(node->value);
-        node->value = strdup(value);
+        node->value = malloc(value_size);
+        if (node->value == NULL) 
+        {
+            perror("Failed to allocate memory for value");
+            return;
+        }
+
+        memcpy(node->value, value, value_size);
+        node->value_size = value_size;
         return;
     }
 
@@ -46,18 +67,39 @@ void setKeyValue(const char* key, const char* value) {
         perror("Failed to allocate memory for node");
         return;
     }
-    strncpy(node->key, key, MAX_KEY_SIZE);
-    node->value = strdup(value);
+
+
+    node->key = malloc(key_size);
+    if (node->key == NULL) {
+        perror("Failed to allocate memory for key");
+        free(node);
+        return;
+    }
+    
+    memcpy(node->key, key, key_size);
+    node->key_size = key_size;
+
+    node->value = malloc(value_size);
+    if (node->value == NULL) {
+        perror("Failed to allocate memory for value");
+        free(node->key);
+        free(node);
+        return;
+    }
+
+    memcpy(node->value, value, value_size);
+    node->value_size = value_size;
     node->next = hashTable[index];
     hashTable[index] = node;
 }
 
-char* getKeyValue(const char* key) {
-    unsigned int index = hash(key);
+void* getKeyValue(const char* key, size_t key_size, size_t* value_size) {
+    unsigned int index = hash(key, key_size);
     node_t* node = hashTable[index];
 
     while (node != NULL) {
-        if (strncmp(node->key, key, MAX_KEY_SIZE) == 0) {
+        if (node->key_size == key_size && memcmp(node->key, key, key_size) == 0) {
+            *value_size = node->value_size;
             return node->value;
         }
         node = node->next;
@@ -65,12 +107,12 @@ char* getKeyValue(const char* key) {
     return NULL;
 }
 
-void delKey(const char* key) {
-    unsigned int index = hash(key);
+void delKey(const char* key, size_t key_size) {
+    unsigned int index = hash(key, key_size);
     node_t* prev = NULL;
     node_t* node = hashTable[index];
 
-    while (node != NULL && strncmp(node->key, key, MAX_KEY_SIZE) != 0) {
+    while (node != NULL && (node->key_size != key_size || memcmp(node->key, key, key_size) != 0)) {
         prev = node;
         node = node->next;
     }
@@ -86,57 +128,134 @@ void delKey(const char* key) {
         prev->next = node->next;
     }
 
+    free(node->key);
     free(node->value);
+    node->key = NULL;
+    node->value = NULL;
     free(node);
 }
 
-void processCommand(int clientSocket, char* command) {
+void processCommand(int clientSocket, char* command) 
+{
     char response[BUFFER_SIZE];
-    if (strncmp(command, "SET", 3) == 0) {
-        char key[MAX_KEY_SIZE], value[BUFFER_SIZE];
-        sscanf(command, "SET %s %s", key, value);
-        setKeyValue(key, value);
+    size_t value_size;
+    size_t key_size;
+    if (strncmp(command, "SET", 3) == 0) 
+    {
+        char key[BUFFER_SIZE];
+        sscanf(command, "SET %s", key);
+        key_size = strlen(key);
+        char* value_str = strchr(command, ' ') + 1;
+        if (strncmp(value_str, "0x", 2) == 0) 
+        {
+            // Convert hexadecimal string to binary data
+            value_size = (strlen(value_str) - 2) / 2;
+            void* value_bin = malloc(value_size);
+            sscanf(value_str + 2, "%*2x%*2x%*2x%*2x%*2x%*2x%*2x%*2x", value_bin);
+            setKeyValue(key, key_size, value_bin, value_size);
+            free(value_bin);
+        } 
+        else 
+        {
+            // Set the value as a string
+            value_size = strlen(value_str);
+            setKeyValue(key, key_size, value_str, value_size);
+        }
         snprintf(response, BUFFER_SIZE, "+OK\r\n");
     }
-    else if (strncmp(command, "GET", 3) == 0) {
-        char key[MAX_KEY_SIZE];
+    else if (strncmp(command, "GET", 3) == 0) 
+    {
+        char key[BUFFER_SIZE];
         sscanf(command, "GET %s", key);
-        char* value = getKeyValue(key);
-        if (value) {
-            snprintf(response, BUFFER_SIZE, "$%s\r\n", value);
+        key_size = strlen(key);
+        void* value = getKeyValue(key, key_size, &value_size);
+
+        if (value) 
+        {
+            // Check if the value is binary
+            int is_binary = 0;
+            for (size_t i = 0; i < value_size; ++i) 
+            {
+                if (!isprint(((char*)value)[i])) 
+                {
+                    is_binary = 1;
+                    break;
+                }
+            }
+
+            if (is_binary) 
+            {
+                // Print binary data as hexadecimal string
+                snprintf(response, BUFFER_SIZE, "$");
+                for (size_t i = 0; i < value_size; i++) 
+                {
+                    snprintf(response + strlen(response), BUFFER_SIZE - strlen(response), "%02x", ((unsigned char*)value)[i]);
+                }
+                snprintf(response + strlen(response), BUFFER_SIZE - strlen(response), "\r\n");
+            }
+            else 
+            {
+                snprintf(response, BUFFER_SIZE, "$%s\r\n", (char*)value);
+            }
         }
-        else {
+        else 
+        {
             snprintf(response, BUFFER_SIZE, "$-1\r\n");
         }
     }
-    else if (strncmp(command, "DEL", 3) == 0) {
-        char key[MAX_KEY_SIZE];
+    else if (strncmp(command, "DEL", 3) == 0) 
+    {
+        char key[BUFFER_SIZE];
         sscanf(command, "DEL %s", key);
-        delKey(key);
+        size_t key_size = strlen(key);
+        delKey(key, key_size);
         snprintf(response, BUFFER_SIZE, "+OK\r\n");
     }
-    else {
+    else 
+    {
         snprintf(response, BUFFER_SIZE, "-ERROR Unknown Command\r\n");
     }
     send(clientSocket, response, strlen(response), 0);
 }
 
-void* handleClient(void* clientSocket) {
-    int sock = *((int*)clientSocket);
-    free(clientSocket);
+void* handleClient(void* arg) 
+{
+    int clientSocket = *(int*)arg;
     char buffer[BUFFER_SIZE];
 
-    while (recv(sock, buffer, BUFFER_SIZE, 0) > 0) {
-        buffer[strlen(buffer) - 1] = '\0'; // Remove newline character
-        processCommand(sock, buffer);
-    }
+    while (1) 
+    {
+        memset(buffer, 0, BUFFER_SIZE);
+        ssize_t numBytes = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0);
 
-    close(sock);
-    return NULL;
+        if (numBytes <= 0) 
+        {
+            // The client has closed the connection or an error occurred
+            if (numBytes == 0) {
+                printf("Client disconnected\n");
+            } else {
+                perror("recv");
+            }
+            close(clientSocket);
+            return NULL;
+        }
+
+        // Process the command and send the response
+        char response[BUFFER_SIZE];
+        processCommand(buffer, response);
+        ssize_t sentBytes = send(clientSocket, response, strlen(response), 0);
+        if (sentBytes < 0) 
+        {
+            perror("send");
+            close(clientSocket);
+            return NULL;
+        }
+    }
 }
 
-int main() {
-    int serverSocket, * clientSocket;
+int main() 
+{
+    int serverSocket, *clientSocket;
     struct sockaddr_in serverAddr, clientAddr;
     socklen_t clientAddrLen = sizeof(clientAddr);
     pthread_t thread;
@@ -181,6 +300,7 @@ int main() {
         pthread_detach(thread);
     }
 
+    
     close(serverSocket);
     return 0;
 }
