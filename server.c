1c5626d4d6f55f304b8725215a48ade5cca8cd0c
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2023-12-19 11:43:17 +0100

    Co-authored-by: Shinzen <Shinzen31@users.noreply.github.com>

--- server.c
+++ server.c
@@ -32,6 +32,28 @@ unsigned int hash(const char* key, size_t key_size) {
     return value % HASH_TABLE_SIZE;
 }
 
+int parseKeyValue(const char* command, char* key, size_t* key_size, char* value, size_t* value_size) {
+
+    const char* ptr = command;
+    while (*ptr && *ptr != ' ') ptr++; // saute "SET"
+    if (!*ptr) return -1;
+    ptr++; // saute " "
+
+    const char* key_start = ptr;
+    while (*ptr && *ptr != ' ') ptr++; // find key end
+    if (!*ptr) return -1;
+    *key_size = ptr - key_start;
+    memcpy(key, key_start, *key_size);
+
+    ptr++; // saute " "
+    const char* value_start = ptr;
+    while (*ptr) ptr++; // until end
+    *value_size = ptr - value_start;
+    memcpy(value, value_start, *value_size);
+
+    return 0;
+}
+
 void setKeyValue(const char* key, size_t key_size, const void* value, size_t value_size) {
     pthread_rwlock_wrlock(&rwlock);
     unsigned int index = hash(key, key_size);
@@ -80,6 +102,20 @@ void setKeyValue(const char* key, size_t key_size, const void* value, size_t val
     node->next = hashTable[index];
     hashTable[index] = node;
     pthread_rwlock_unlock(&rwlock);
+     // Stockage de stockage fiable des clefs sur le disque (=persistance)
+    FILE *file = fopen("datafile.bin", "ab"); 
+    if (file == NULL) {
+        perror("Failed to open file");
+        return;
+    }
+
+    fwrite(&key_size, sizeof(size_t), 1, file);
+    fwrite(key, key_size, 1, file);
+    
+    fwrite(&value_size, sizeof(size_t), 1, file);
+    fwrite(value, value_size, 1, file);
+
+    fclose(file);
 }
 
 void* getKeyValue(const char* key, size_t* value_size) {
@@ -111,6 +147,7 @@ int delKey(const char* key, size_t key_size) {
     }
 
     if (node == NULL) {
+        pthread_rwlock_unlock(&rwlock);
         return 0;
     }
 
@@ -124,7 +161,131 @@ int delKey(const char* key, size_t key_size) {
     free(node->key);
     free(node->value);
     free(node);
-    return 1;
+    
+     // Remove the key from the file
+    FILE *file = fopen("datafile.bin", "rb+");
+    if (file == NULL) 
+    {
+        perror("Failed to open file for deletion");
+        pthread_rwlock_unlock(&rwlock);
+        return 0;  // File could not be opened
+    }
+
+    size_t current_key_size;
+    size_t current_value_size;
+    char current_key[BUFFER_SIZE];
+    void *current_value;
+
+    while (fread(&current_key_size, sizeof(size_t), 1, file) == 1) 
+    {
+        fread(current_key, current_key_size, 1, file);
+        fread(&current_value_size, sizeof(size_t), 1, file);
+
+        current_value = malloc(current_value_size);
+        if (current_value == NULL) 
+        {
+            perror("Failed to allocate memory for current value");
+            fclose(file);
+            pthread_rwlock_unlock(&rwlock);
+            return 0;  // Memory allocation failed
+        }
+
+        fread(current_value, current_value_size, 1, file);
+
+        if (current_key_size == key_size && memcmp(current_key, key, key_size) == 0) 
+        {
+            // Key found, don't write it back to the file
+            free(current_value);
+            break;
+        }
+
+        // Write the key back to the file
+        fwrite(&current_key_size, sizeof(size_t), 1, file);
+        fwrite(current_key, current_key_size, 1, file);
+        fwrite(&current_value_size, sizeof(size_t), 1, file);
+        fwrite(current_value, current_value_size, 1, file);
+
+        free(current_value);
+    }
+
+    fclose(file);
+    pthread_rwlock_unlock(&rwlock);
+    return 1; // Key deleted
+}
+
+int keyExists(const char* key) 
+{
+    pthread_rwlock_rdlock(&rwlock);
+    size_t key_size = strlen(key);
+
+    unsigned int index = hash(key, key_size);
+    node_t* node = hashTable[index];
+
+    while (node != NULL) 
+    {
+        if (node->key_size == key_size && strncmp(node->key, key, key_size) == 0) 
+        {
+            pthread_rwlock_unlock(&rwlock);
+            return 1;  // Key exists
+        }
+        node = node->next;
+    }
+
+    pthread_rwlock_unlock(&rwlock);
+    return 0;  // Key does not exist
+}
+
+void renameKey(const char* old_key, size_t old_key_size, const char* new_key, size_t new_key_size) {
+    pthread_rwlock_wrlock(&rwlock);
+
+    // chercher le key
+    unsigned int old_index = hash(old_key, old_key_size);
+    node_t* prev_node = NULL;
+    node_t* old_node = hashTable[old_index];
+    while (old_node != NULL && (old_node->key_size != old_key_size || strncmp(old_node->key, old_key, old_key_size) != 0)) {
+        prev_node = old_node;
+        old_node = old_node->next;
+    }
+
+    if (old_node == NULL) {
+        pthread_rwlock_unlock(&rwlock);
+        printf("Old key not found\n");
+        return;
+    }
+
+    // creer nouvelle key
+    unsigned int new_index = hash(new_key, new_key_size);
+    node_t* new_node = malloc(sizeof(node_t));
+    if (new_node == NULL) {
+        pthread_rwlock_unlock(&rwlock);
+        perror("Failed to allocate memory for new node");
+        return;
+    }
+
+    new_node->key = malloc(new_key_size);
+    if (new_node->key == NULL) {
+        pthread_rwlock_unlock(&rwlock);
+        perror("Failed to allocate memory for new key");
+        free(new_node);
+        return;
+    }
+    memcpy(new_node->key, new_key, new_key_size);
+    new_node->key_size = new_key_size;
+    new_node->value = old_node->value; 
+    new_node->value_size = old_node->value_size;
+    new_node->next = hashTable[new_index];
+    hashTable[new_index] = new_node;
+
+    // supprime node ancienne
+    if (prev_node == NULL) {
+        hashTable[old_index] = old_node->next;
+    } else {
+        prev_node->next = old_node->next;
+    }
+    free(old_node->key);
+    free(old_node); 
+
+    pthread_rwlock_unlock(&rwlock);
 }
 
 void processCommand(int clientSocket, char* command) {
@@ -133,21 +294,20 @@ void processCommand(int clientSocket, char* command) {
     size_t value_size;
 
     if (strncmp(command, "SET", 3) == 0) {
-        pthread_mutex_lock(&lock);
+    pthread_mutex_lock(&lock);
 
-        char key[BUFFER_SIZE];
-        char value[BUFFER_SIZE];
+    char key[BUFFER_SIZE];
+    char value[BUFFER_SIZE];
+    size_t key_size, value_size;
 
-        if (sscanf(command, "SET %s %s", key, value) != 2) {
-            snprintf(response, BUFFER_SIZE, "-ERROR Invalid SET command format\r\n");
-        } else {
-            size_t key_size = strlen(key);
-            size_t value_size = strlen(value);
-            setKeyValue(key, key_size, value, value_size);
-            snprintf(response, BUFFER_SIZE, "+OK\r\n");
-        }
+    if (parseKeyValue(command, key, &key_size, value, &value_size) != 0) {
+        snprintf(response, BUFFER_SIZE, "-ERROR Invalid SET command format\r\n");
+    } else {
+        setKeyValue(key, key_size, value, value_size);
+        snprintf(response, BUFFER_SIZE, "+OK\r\n");
+    }
 
-        pthread_mutex_unlock(&lock);
+    pthread_mutex_unlock(&lock);
     }
     else if (strncmp(command, "GET", 3) == 0) {
     char key[BUFFER_SIZE];
@@ -196,6 +356,36 @@ void processCommand(int clientSocket, char* command) {
         close(clientSocket);
         pthread_exit(NULL);
     }
+    else if (strncmp(command, "RENAME", 6) == 0) {
+    char old_key[BUFFER_SIZE];
+    char new_key[BUFFER_SIZE];
+    if (sscanf(command, "RENAME %s %s", old_key, new_key) == 2) {
+        pthread_mutex_lock(&lock);
+        size_t old_key_size = strlen(old_key);
+        size_t new_key_size = strlen(new_key);
+        renameKey(old_key, old_key_size, new_key, new_key_size);
+        snprintf(response, BUFFER_SIZE, "+OK\r\n");
+        pthread_mutex_unlock(&lock);
+    } else {
+        snprintf(response, BUFFER_SIZE, "-ERROR Invalid RENAME command format\r\n");
+    }
+}
+
+    else if (strncmp(command, "EXISTS", 6) == 0) 
+    {
+        char key[BUFFER_SIZE];
+        if (sscanf(command + 6, " %s", key) == 1) 
+        {
+            pthread_mutex_lock(&lock);
+            int exists = keyExists(key);
+            snprintf(response, BUFFER_SIZE, ":%d\r\n", exists);  // Return 1 if key exists, 0 otherwise
+            pthread_mutex_unlock(&lock);
+        } 
+        else 
+        {
+            snprintf(response, BUFFER_SIZE, "-ERROR Invalid EXISTS command format\r\n");
+        }
+    }
     else {
         snprintf(response, BUFFER_SIZE, "-ERROR Unknown Command\r\n");
     }
@@ -215,6 +405,14 @@ void* handleClient(void* arg) {
         if (numBytes <= 0) {
             if (numBytes == 0) {
                 printf("Client disconnected\n");
+                if (remove("datafile.bin") == 0) 
+                {
+                    printf("File deleted successfully.\n");
+                }  
+                else 
+                {
+                    perror("Error deleting file");
+                }
             } else {
                 perror("recv");
             }
